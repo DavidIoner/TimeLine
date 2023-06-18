051c768873f7b7227b26fc14a71c20aa7ffd76a4
DavidIoner <davidhioner@gmail.com>
2023-06-18 19:15:31 -0300

    tailwind colors

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -4,8 +4,9 @@ import { Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-slate-500">
-      <Text>Hello world!</Text>
+    <View className="flex-1 items-center justify-center bg-gray-800">
+      <Text className='text-5xl font-bold text-gray-50'>Novo App</Text>
+      <Text className='text-2xl font-bold text-center text-gray-50'>aqui começo uma nova jornada!</Text>
       <StatusBar style="auto" translucent />
     </View>
   );
